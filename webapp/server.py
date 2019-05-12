d14cb8bebe96249bf36eb8591b786e0f2861a89e
tahtaciburak <tahtaciburak@gmail.com>
2019-05-12 14:05:28 +0300

    web app added

--- webapp/server.py
+++ webapp/server.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return 'merhaba flask'
+    return render_template('index.html')
 
+@app.route("/crime")
+def get_crime_data():
+    
 if __name__ == "__main__":
     app.run()
